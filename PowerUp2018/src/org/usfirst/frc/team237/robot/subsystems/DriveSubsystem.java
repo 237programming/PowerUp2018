65c9d05d4d2ef7659219a7dd2a1ec5a79c182098
Bethany B <Bethany B@192.168.1.109>
2018-01-11 19:49:33 -0500

    Created DriveSubsystem method and set Talon Modes fro Drive

--- PowerUp2018/src/org/usfirst/frc/team237/robot/subsystems/DriveSubsystem.java
+++ PowerUp2018/src/org/usfirst/frc/team237/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team237.robot.subsystems;
 
 
 import org.usfirst.frc.team237.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
 import com.kauailabs.navx.frc.AHRS;
 
@@ -19,17 +21,19 @@ public class DriveSubsystem extends Subsystem
     // here. Call these from Commands.
 	
 	private TalonSRX leftDrive = new TalonSRX(RobotMap.driveTalon1);
-	private TalonSRX leftDriveSlave1 = new TalonSRX(RobotMap.driveTalon2);
-	private TalonSRX leftDriveSlave2 = new TalonSRX(RobotMap.driveTalon3);
-	private TalonSRX leftDriveSlave3 = new TalonSRX(RobotMap.driveTalon4);
-	private TalonSRX rightDrive = new TalonSRX(RobotMap.driveTalon5);
-	private TalonSRX rightDriveSlave1 = new TalonSRX(RobotMap.driveTalon6);
-	private TalonSRX rightDriveSlave2 = new TalonSRX(RobotMap.driveTalon7);
-	private TalonSRX rightDriveSlave3 = new TalonSRX(RobotMap.driveTalon8);
+	private TalonSRX leftDriveSlave = new TalonSRX(RobotMap.driveTalon2);
+	private TalonSRX rightDrive = new TalonSRX(RobotMap.driveTalon3);
+	private TalonSRX rightDriveSlave = new TalonSRX(RobotMap.driveTalon4);
 	private AHRS gyro = new AHRS(SerialPort.Port.kUSB, AHRS.SerialDataType.kProcessedData, (byte) 200);
     
 	public DriveSubsystem()
 	{
+		leftDrive.set(ControlMode.PercentOutput, 0);
+		leftDriveSlave.set(ControlMode.Follower, 0);
+		rightDrive.set(ControlMode.PercentOutput, 0);
+		rightDriveSlave.set(ControlMode.Follower, 0);
+		
+		gyro.reset();
 		
 	}
     public void initDefaultCommand() 
