a0bcdeaaa292b0812797c7550423653a4fef39dd
doola <doola@192.168.1.100>
2018-01-12 17:48:51 -0500

    Added on/off methods for climber subsystem

--- PowerUp2018/src/org/usfirst/frc/team237/robot/subsystems/ClimberSubsystem.java
+++ PowerUp2018/src/org/usfirst/frc/team237/robot/subsystems/ClimberSubsystem.java
@@ -2,7 +2,8 @@ package org.usfirst.frc.team237.robot.subsystems;
 
 import org.usfirst.frc.team237.robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -10,16 +11,32 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class ClimberSubsystem extends Subsystem {
+public class ClimberSubsystem extends Subsystem 
+{
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	private TalonSRX climbMotor = new TalonSRX(RobotMap.climber);
+	private WPI_TalonSRX climbMotor = new WPI_TalonSRX(RobotMap.climber);
 	private Compressor compressor = new Compressor();
-    public void initDefaultCommand() {
+    public void initDefaultCommand() 
+    {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    }
+    }	
+    public void ClimberSubsystem()
+   	{
+  		climbMotor.set(ControlMode.PercentOutput, 0);
+   	}
+    public void climberOn()
+   	{
+   		climbMotor.set(1);
+   	}
+   	public void climberOff()
+   	{
+   		climbMotor.set(0);
+   	}
+    		
 }
 
 // This is officially the dankest line of code
+
